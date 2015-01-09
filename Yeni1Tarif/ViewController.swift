b04fd182c1b0f9d815dff436cb23692b1b0341b6
Ali Servet Donmez <asd@pittle.org>
2015-01-09 23:30:06 +0100

    Initial commit.

---
+++ Yeni1Tarif/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yeni1Tarif
+//
+//  Created by Ali Servet Donmez on 9.01.2015.
+//  Copyright (c) 2015 Ali Servet Donmez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
