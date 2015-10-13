fe0f42e7e3df7b163ed27d6000c7e311d9ad99c3
Ali Servet Donmez <asd@pittle.org>
2015-10-13 23:40:28 +0200

    Download scaled down sized images for faster UX

--- app/data_sources/EntriesViewDataSource.UICollectionView.swift
+++ app/data_sources/EntriesViewDataSource.UICollectionView.swift
@@ -21,8 +21,19 @@ extension EntriesViewDataSource : UICollectionViewDataSource {
 
     guard let entry = self.entryAtIndexPath(indexPath) else { return cell }
 
-    let url = NSURL(string: entry.previewUrl)
-    cell.previewImageView.sd_setImageWithURL(url)
+    switch(indexPath.row) {
+
+    case 0:
+      let previewUrl = entry.previewUrl
+      let url = NSURL(string: previewUrl)
+      cell.previewImageView.sd_setImageWithURL(url)
+
+    default:
+      let previewUrl = entry.previewUrl.stringByReplacingOccurrencesOfString("1024x1024", withString: "150x150")
+      let url = NSURL(string: previewUrl)
+      cell.previewImageView.sd_setImageWithURL(url)
+
+    }
 
     return cell
   }
